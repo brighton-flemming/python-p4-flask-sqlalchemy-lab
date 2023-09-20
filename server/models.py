8058cd2eb54d2830735fd7381bb4b4cb55f42546
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-20 20:23:58 +0300

    Mission Passed

--- server/models.py
+++ server/models.py
@@ -11,13 +11,23 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    birthday = db.Column(db.Date)
+    animals = db.Column(db.String)
+
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    species = db.Column(db.String)
+    enclosure  = db.Column(db.String)
+
